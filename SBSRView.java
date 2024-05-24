a6174f719b39fba4cf789be8e8eaa84958a50663
BoscoZhangers <168551399+BoscoZhangers@users.noreply.github.com>
2024-05-24 09:41:49 -0400

    Update SBSRView.java

--- SBSRView.java
+++ SBSRView.java
@@ -1,1 +1,15 @@
-import 
+//SuperBrawlStarsRun SBSR View (Graphical User Interfaces)
+/Authors: Bosco Zhang, Liam Ma, Nihal Sidhu
+//Last Modified: Friday, May 24, 2024
+//Version Number: 2.0 Beta
+
+//Importing Java Swing and GUI toolkits for graphical setup
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+
+//Defining
+
+ 
